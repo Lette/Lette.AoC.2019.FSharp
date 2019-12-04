0455aa10e637e371ea82922e0815f2c2403908c7
Christoffer Lette <christoffer.lette@gmail.com>
2019-12-04 21:13:27 +0100

    Solved day 4.

--- AoC/Day04.fs
+++ AoC/Day04.fs
@@ -3,7 +3,7 @@ module Day04
 
     let xs = (165432, 707912)
 
-    let isValid n =
+    let meetsFirstCriteria n =
         let d6 = n % 10
         let q6 = n / 10
         let d5 = q6 % 10
@@ -13,29 +13,54 @@ module Day04
         let d3 = q4 % 10
         let q3 = q4 / 10
         let d2 = q3 % 10
-        let q2 = q3 / 10
-        let d1 = q2
+        let d1 = q3 / 10
 
         ((d1 = d2) || (d2 = d3) || (d3 = d4) || (d4 = d5) || (d5 = d6))
         &&
         ((d1 <= d2) && (d2 <= d3) && (d3 <= d4) && (d4 <= d5) && (d5 <= d6))
 
+    let firstPass =
+        lazy (
+            [ (fst xs) .. (snd xs) ]
+            |> List.filter meetsFirstCriteria
+        )
+
     let part1 () =
 
-        let last = (snd xs) + 1
+        firstPass.Value
+        |> List.length
 
-        let rec run n acc =
-            match n, isValid n with
-            | (x, _) when x = last -> acc
-            | _, v                 -> run (n + 1) (if v then acc + 1 else acc)
+    let meetsSecondCriteria n =
+        let d6 = n % 10
+        let q6 = n / 10
+        let d5 = q6 % 10
+        let q5 = q6 / 10
+        let d4 = q5 % 10
+        let q4 = q5 / 10
+        let d3 = q4 % 10
+        let q3 = q4 / 10
+        let d2 = q3 % 10
+        let d1 = q3 / 10
 
-        run (fst xs) 0
+        (
+            ((d1 = d2)                  && not (d2 = d3))
+            ||
+            ((d2 = d3) && not (d1 = d2) && not (d3 = d4))
+            ||
+            ((d3 = d4) && not (d2 = d3) && not (d4 = d5))
+            ||
+            ((d4 = d5) && not (d3 = d4) && not (d5 = d6))
+            ||
+            ((d5 = d6) && not (d4 = d5)                 )
+        )
 
     let part2 () =
-        0
+        firstPass.Value
+        |> List.filter meetsSecondCriteria
+        |> List.length
 
     let show () =
         showDay
             4
             part1 (Some 1716)
-            part2 None
+            part2 (Some 1163)
