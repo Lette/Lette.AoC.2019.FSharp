9716c4428838ee962db4403a3ac8f318e82139f3
Christoffer Lette <christoffer.lette@gmail.com>
2019-12-06 17:31:25 +0100

    Solved part 2 of day 6.

--- AoC/Day06.fs
+++ AoC/Day06.fs
@@ -11,26 +11,52 @@ module Day06
         |> List.map toOrbit
 
     let part1 () =
-        
-        let rec runOrbits leaves newLeaves weight acc =
-            
-            match leaves, newLeaves with
-            | [], []     -> acc
-            | [], _      -> runOrbits newLeaves [] (weight + 1) acc
-            | l :: ls, _ ->
-                let subOrbits =
+
+        let rec runOrbits parents children weight acc =
+
+            match parents, children with
+            | []     , [] -> acc
+            | []     , _  -> runOrbits children [] (weight + 1) acc
+            | p :: ps, _  ->
+                let newChildren =
                     orbits
-                    |> List.filter (fst >> ((=) l))
+                    |> List.filter (fst >> (=) p)
                     |> List.map snd
-                runOrbits ls (subOrbits @ newLeaves) weight (acc + weight * (List.length subOrbits))
+                runOrbits ps (newChildren @ children) weight (acc + weight * (List.length newChildren))
  
         runOrbits ["COM"] [] 1 0
 
     let part2 () =
-        0
+
+        let findParent child =
+            orbits
+            |> List.find (snd >> (=) child)
+            |> fst
+
+        let rec stripEqualElementsFromStart a b =
+            match a, b with
+            | [], _                        -> (a, b)
+            | _ , []                       -> (a, b)
+            | x :: _ , y :: _  when x <> y -> (a, b)
+            | _ :: xs, _ :: ys             -> stripEqualElementsFromStart xs ys
+
+        let findFullPath child =
+
+            let rec run child acc =
+                match child with
+                | "COM" -> acc
+                | _     ->
+                    let o = findParent child
+                    run o (o :: acc)
+
+            run child [child]
+
+        (findFullPath "YOU", findFullPath "SAN")
+        ||> stripEqualElementsFromStart
+        ||> fun a b -> List.length a + List.length b - 2
 
     let show () =
         showDay
             6
             part1 (Some 227612)
-            part2 None
+            part2 (Some 454)
